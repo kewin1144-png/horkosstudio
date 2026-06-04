4393e91f91bd9de75e5a13a64825a3f77f8fc9a9
kewin1144-png <kewin1144@gmail.com>
2026-06-04 20:57:55 +0530

    Add files via upload

---
+++ update_link.py
@@ -0,0 +1,8 @@
+import re
+with open('about.html', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+text = text.replace('href="https://www.edwinpaleti.netlify.app"', 'href="https://edwinpaleti.netlify.app/"')
+
+with open('about.html', 'w', encoding='utf-8') as f:
+    f.write(text)
